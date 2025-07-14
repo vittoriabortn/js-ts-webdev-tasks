40454e47a58d5948102fe143749d27cd8ecaa07a
vittoriabortn <viktoria@MacBookAir.home>
2025-07-14 19:12:26 +0200

    5

--- hometask-shop-co/src/pages/CatalogPage.ts
+++ hometask-shop-co/src/pages/CatalogPage.ts
@@ -1,13 +1,18 @@
 import { getProducts } from '../api/products';
 import { ProductGrid } from '../components/product-grid/product-grid';
 import { Layout } from '../layout/Layout';
+import { SidebarFilter } from '../components/sidebar-filter/sidebar-filter';
+import '../components/sidebar-filter/sidebar-filter.css';
 
 export const CatalogPage = async (): Promise<string> => {
   const products = await getProducts();
 
   return Layout(`
-    <section class="products">
-      ${ProductGrid(products)}
-    </section>
+    <div class="catalog">
+      ${SidebarFilter()}
+      <section class="products">
+        ${ProductGrid(products)}
+      </section>
+    </div>
   `);
 };
